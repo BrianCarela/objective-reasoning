1c8df629ce59c039cf2ad60fd8158bd388231484
BrianCarela <briancarela@gmail.com>
2021-03-15 10:06:50 -0400

    another solution

--- main.js
+++ main.js
@@ -1,46 +1,47 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
-
-function isAdmin(user) {
-    if (user.userRole === "ADMIN") {
-        return true;
-    } else {
-        return false;
-    }
+function isAdmin (user){
+    return user.userRole === 'ADMIN'
 }
 
-function getEmail(user) {
-    return user.firstName.concat(".", user.lastName, "@codeimmersives.com").toLowerCase();
+function getEmail(user){
+    return (user.firstName + '.' +user.lastName + "@codeimmersives.com").toLowerCase()
 }
 
-function getPlaylistLength(list) {
-    return list.songs.length;
+function getPlaylistLength(playList){
+    return playList.songs.length
+
 }
 
-function getHardestHomework(hardest) {
-    //define variables to act as placeholders for lowest scores and the name of that hw with the lowest score
-    let lowestScore = 999999999;
-    let nameOfHardestHw = "";
-    //loops through array of objects and if score is lower than lowestScore it will store score each iteration of loop in lowestScore variable and will also push the name of it to nameOfHardestHw.
-    for (const item of hardest) {
-        if (item.averageScore < lowestScore) {
-            lowestScore = item.averageScore;
-            nameOfHardestHw = item.name;
+function getHardestHomework(arrObj){
+    if (arrObj.length === 0) {return ""}
+
+    let hardestHW = arrObj[0].name
+    let minScore = arrObj[0].averageScore
+
+
+    for (const eachObj of arrObj){
+        if (eachObj.averageScore < minScore){  
+            minScore =  eachObj.averageScore 
+            hardestHW =  eachObj.name   
         }
     }
-    //returns the name of the hardest hw
-    return nameOfHardestHw;
+    return hardestHW
 }
 
-function createPhonebook(name, number) {
-    //defines variable to an empty object that will store solution
-    let final = {};
-    //loops through both name and number arrays with forEach method and inserts name as keys and number as values
-    name.forEach((name, i) => final[name] = number[i])
-    //returns object
-    return final;
+function createPhonebook(name, number){
+    const phoneBook = {
+    }
+        for (let i = 0;i < name.length; i++){
+        phoneBook[name[i]] = number[i]
+        }
+    console.log(phoneBook)
+    return phoneBook
 }
+
+
+
 
 
 // ┌─────────────────────────────────────┐
